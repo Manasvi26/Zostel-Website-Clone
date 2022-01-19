5dc0bfce504342b161e2c9545c0f379741b811ab
Manasvi Sharma <manasvi.manu94@gmail.com>
2022-01-19 15:04:24 +0530

    Adding data file

---
+++ client/src/Constants/data.js
@@ -0,0 +1,25 @@
+export const destinations = [
+  {
+    url: "",
+    name: "",
+  },
+  {
+    url: "",
+    name: "",
+  },
+];
+
+export const hotels = [
+  {
+    url: "",
+    status: "LAUNCH",
+    title: "ZOSTEL - BACK IN DALHOUSIE",
+    description: "",
+  },
+  {
+    url: "",
+    status: "LAUNCH",
+    title: "ZOSTEL - BACK IN DALHOUSIE",
+    description: "",
+  },
+];
