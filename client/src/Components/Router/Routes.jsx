4f3999e61818140762990941ddc46efa3c46c65a
Vaibhav Kothiyal <vaibhavkkothiyal@gmail.com>
2022-01-22 12:20:26 +0530

    update

--- client/src/Components/Router/Routes.jsx
+++ client/src/Components/Router/Routes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { PreSelDestination } from "../Pages/PreSelectedDestination/PreSelDestination";
 import { SelectedDestination } from "../Pages/SelectedDestination/SelectedDest";
 import {Home} from '../Pages/Home/Home'
+import {Details} from '../Pages/Details/Details'
 
 export const DirectRoutes=()=>{
     return (
@@ -9,6 +10,7 @@ export const DirectRoutes=()=>{
             <Route path="/" element={<Home />}></Route>
             <Route path="/predestination" element={<PreSelDestination />}></Route>
             <Route path="/destination" element={<SelectedDestination />}></Route>
+            <Route path="/details" element={<Details />}></Route>
         </Routes>
     )
 }
